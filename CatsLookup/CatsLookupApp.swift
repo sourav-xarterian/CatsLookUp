ab446fac13d3e6347e616c9c68c670d939de7da3
Sourav Maharana <sourav.maharana@xarterian.com>
2025-10-28 11:58:52 +0530

    Initial Commit

---
+++ CatsLookup/CatsLookupApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatsLookupApp.swift
+//  CatsLookup
+//
+//  Created by Sourav Maharana on 28/10/25.
+//
+
+import SwiftUI
+
+@main
+struct CatsLookupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
