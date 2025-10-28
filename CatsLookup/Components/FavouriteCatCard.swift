d9dc48b57dc698ef3290b1ba5eb92dfb408aed55
Sourav Maharana <sourav.maharana@xarterian.com>
2025-10-28 14:09:36 +0530

    Developed Static Favourite Cat UI Card

--- CatsLookup/Components/FavouriteCatCard.swift
+++ CatsLookup/Components/FavouriteCatCard.swift
@@ -9,15 +9,20 @@ import SwiftUI
 
 struct FavouriteCatCard: View {
     var body: some View {
-        VStack(alignment: .leading, spacing: 4) {
-            Text("Cat Name Goes here")
-            Text("Cat Description: Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.")
-                .lineLimit(nil)
+        HStack(alignment: .center, spacing: 12) {
+            VStack(alignment: .leading, spacing: 8) {
+                Text("Cat Name Goes here")
+                Text("Cat Description: Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.")
+                    .lineLimit(nil)
+            }
+            Image(systemName: "heart")
+                .font(.system(size: 22))
+                .padding(.trailing, 4)
         }
         .padding(6)
     }
 }
 
-#Preview(traits: .fixedLayout(width: 400, height:  120)) {
+#Preview(traits: .fixedLayout(width: 400, height:  150)) {
     FavouriteCatCard()
 }
